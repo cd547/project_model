6957eb91e3c2bc478fe757a839cfda3b9e8daed2
547 <415963856@qq.com>
2018-06-27 13:37:32 +0800

    跳转具体项目1级界面

--- application/controllers/ProjectController.php
+++ application/controllers/ProjectController.php
@@ -4,10 +4,10 @@ require_once APPLICATION_PATH.'/models/users.php';//model table
 require_once APPLICATION_PATH.'/models/pro_level1.php';//pro_level1表
 
 /*控制器用于响应登录*/
-class MainController extends BaseController
+class ProjectController extends BaseController
 {
-    //主页
-	public function indexAction()
+    //项目主页
+	public function prolevel1Action()
     {
         if (!session_id())session_start();
         if (!isset($_SESSION['loginuser']))
@@ -15,20 +15,18 @@ class MainController extends BaseController
             $this->redirect('/index/index');
             exit();
         }
-        $query=$this->getRequest()->getParam("query","");
+        $pro_num=$this->getRequest()->getParam("pro_num","");
         //获取项目信息
         $pro_level1=new pro_level1();
         $db=$pro_level1->getAdapter();
-        $sql=$db->select()->from('pro_level1','*')->order('Sys_id ASC');
-        $paginator = Zend_Paginator::factory($sql);
-        $paginator->setItemCountPerPage(5);//每页行数
-        $paginator->setPageRange(10);
-        $paginator->setCurrentPageNumber($this->_getParam('page'));
-        $this->view->paginator = $paginator;
-        $this->view->requestParams=$query;
+        $sql=$db->select()->from('pro_level1','*')->where('pro_num=?',$pro_num);
+        $result= $db->query($sql);
+        $rows=$result->fetchAll();
+        //项目信息
+        $this->view->project_data=$rows;
         //用户信息
         $this->view->loginuser=$_SESSION['loginuser'];
-        $this->render();
+        $this->render('prolevel1');
     }
 
 //跳转主页
@@ -68,12 +66,10 @@ class MainController extends BaseController
         $paginator->setCurrentPageNumber($this->_getParam('page'));
         $this->view->paginator = $paginator;
         $this->view->requestParams=$query;
-        //用户信息
-        $this->view->loginuser=$_SESSION['loginuser'];
         $this->render('ajaxpage');
     }
 
-
+   
 
 }
 
